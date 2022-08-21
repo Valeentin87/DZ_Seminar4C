66ec75c7fb4dcffdcf88c2a8c1eff4520a6c6667
Valeentin87 <Valeentin87@list.ru>
2022-08-21 17:56:42 +0300

    Успешное решение задачи 27

--- Zadacha27/Program.cs
+++ Zadacha27/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"сумма цифр в числе {num} равна {SumOfDigits(num)}");
+
+int SumOfDigits(int number)
+{
+int sum = 0;
+while (number!=0)
+{
+    if (number>0 && number<10)
+    {
+        sum = sum + number;
+        number = 0;
+    }
+    else
+    {
+        sum = sum + number%10;
+        number = number/10;
+    }
+    
+}
+    return sum;
+}
+
