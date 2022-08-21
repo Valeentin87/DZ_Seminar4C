88feb9b041dd392dd338ced46d3a09169a710687
Valeentin87 <Valeentin87@list.ru>
2022-08-21 18:49:53 +0300

    успешное решение задачи 29

--- Zadacha29/Program.cs
+++ Zadacha29/Program.cs
@@ -1,7 +1,11 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 Console.WriteLine("введите количество членов массива");
-int N = Console.ReadLine();
+int N = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(ArrayPrint(N));
+for(int j=0; j< ArrayPrint(N).Length;j++)
+{
+    Console.Write($"{ArrayPrint(N)[j]} , ");
+}
 double [] ArrayPrint(int number)
 {
     double [] newArray = new double[number];
