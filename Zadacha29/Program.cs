c379f3035e8d4ed8b5d5f99092d4bd1be425c266
Valeentin87 <Valeentin87@list.ru>
2022-08-21 18:33:46 +0300

    создали проект для решения задачи 29

--- Zadacha29/Program.cs
+++ Zadacha29/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.WriteLine("введите количество членов массива");
+int N = Console.ReadLine();
+Console.WriteLine(ArrayPrint(N));
+double [] ArrayPrint(int number)
+{
+    double [] newArray = new double[number];
+    Random random = new Random();
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        double newArr = Convert.ToDouble(random.Next(-100,100));
+        newArray[i] = newArr;
+    }
+    return newArray;
+}
